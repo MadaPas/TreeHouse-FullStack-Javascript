7a6454cfd2b42fcc25d34dd46f4491dda1d27df5
Madalina - Andreea <mada0193@stud.kea.dk>
2020-12-29 15:10:32 +0100

    selector last task

---
+++ 3.DOM/project/selector-dom-task/app.js
@@ -0,0 +1,3 @@
+let navigationLinks = document.querySelectorAll('nav > ul > li > a');
+let galleryLinks = document.querySelectorAll('ul#gallery > a') ;
+let footerImages = document.querySelectorAll('footer img');
