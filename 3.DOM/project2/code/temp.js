95382a59d8b2d7aedfe16c045447612d92ecb6f6
Madalina - Andreea <mada0193@stud.kea.dk>
2020-12-29 23:48:43 +0100

    DOM - continue with the course

--- 3.DOM/project2/code/temp.js
+++ 3.DOM/project2/code/temp.js
@@ -7,7 +7,13 @@ say('Hello');
 function exec(func, arg) {
     func(arg);
 }
+exec(say, "hello!!")
 
 exec((something) => {
     console.log(something);
 }, 'Greetings, everyone!');
+
+
+window.setTimeout((something) => {
+    console.log(something);
+}, 5000, 'Greetings, everyone!');
