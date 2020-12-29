21b8b60e90c7403d32aad4a67c9cd8a2393c2798
Madalina - Andreea <mada0193@stud.kea.dk>
2020-12-29 23:04:17 +0100

    DOM - part 3 - first proj

--- 3.DOM/project2/code/app.js
+++ 3.DOM/project2/code/app.js
@@ -13,6 +13,9 @@ const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 
+const removeItemButton = document.querySelector('button.removeItemButton');
+
+
 toggleList.addEventListener('click', () => { 
   if (listDiv.style.display == 'none') {
     toggleList.textContent = 'Hide list';
@@ -63,6 +66,12 @@ addItemButton.addEventListener('click', () => {
    li.textContent = addItemInput.value;
    ul.appendChild(li);
    addItemInput.value = '';
+});
+
+removeItemButton.addEventListener('click', () => {
+   let ul = document.getElementsByTagName('ul')[0];
+   let li = document.querySelector('li:last-child');
+   ul.removeChild(li);
 });
 
 
