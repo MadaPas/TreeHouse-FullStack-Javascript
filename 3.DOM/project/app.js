c1849ec4f554f9794bbf5c896de56ce56f2f5371
Madalina - Andreea <mada0193@stud.kea.dk>
2020-12-29 14:48:38 +0100

    treehouse workspace on DOM - continue course

--- 3.DOM/project/app.js
+++ 3.DOM/project/app.js
@@ -21,11 +21,39 @@ for(let i = 0; i< myList.length; i += 1) {
 }
 
 //
-//next task (getElementsByClassName )
+//next task (getElementsByclassName )
 //
 
-const errorNotPurple = document.getElementsByClassName('error-not-purple');
+//const errorNotPurple = document.getElementsByClassName('error-not-purple');
+//
+//for(let i = 0; i< errorNotPurple.length; i += 1) {
+//  errorNotPurple[i].style.color = 'red';
+//}
 
+//
+//next task (document.querySelector)
+//
+
+//document.querySelectorAll('li')
+//document.querySelector('li')
+//document.querySelector('#myHeading')
+//document.querySelectorAll('.error-not-purple')
+//document.querySelector('[title=label]')
+
+const errorNotPurple = document.querySelectorAll('.error-not-purple');
 for(let i = 0; i< errorNotPurple.length; i += 1) {
   errorNotPurple[i].style.color = 'red';
 }
+
+const evens = document.querySelectorAll('li:nth-child(even)');
+
+for(let j = 0; j< evens.length; j += 1) {
+  evens[j].style.backgroundColor = 'lightgrey';
+}
+
+const odds = document.querySelectorAll('li:nth-child(odd)');
+
+for(let k = 0; k< odds.length; k += 1) {
+  odds[k].style.backgroundColor = 'lightblue';
+}
+console.log()
