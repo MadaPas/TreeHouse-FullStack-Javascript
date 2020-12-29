2ae37af05f33b9b5746d436f526406b0e6026375
Madalina - Andreea <mada0193@stud.kea.dk>
2020-12-29 19:58:08 +0100

    project 2 task-2

---
+++ 3.DOM/project2/task-2/app.js
@@ -0,0 +1,6 @@
+var contentDiv = document.getElementById('content');
+var newParagraph = document.createElement('p');
+newParagraph.className = 'panel';
+contentDiv.appendChild(newParagraph);
+newParagraph.classList.add('panel');
+
