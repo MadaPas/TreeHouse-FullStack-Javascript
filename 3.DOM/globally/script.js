bbb3ba9f85b60160f6d049915393754ed15a1600
Madalina - Andreea <mada0193@stud.kea.dk>
2020-12-29 14:31:51 +0100

    DOM starting

---
+++ 3.DOM/globally/script.js
@@ -0,0 +1,8 @@
+alert('I made the browser message me');
+
+location.href 
+
+document.getElementById('myHeading').style.color = 'purple'
+
+document.getElementById('myHeading').style.background = 'yellow'
+
