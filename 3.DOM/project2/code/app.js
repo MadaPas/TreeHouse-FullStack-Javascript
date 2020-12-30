b87d8e2eecacbae94e39e4dc8511d7b086276cc1
Madalina - Andreea <mada0193@stud.kea.dk>
2020-12-30 10:43:47 +0100

    DOM - task 7 + continuing

--- 3.DOM/project2/code/app.js
+++ 3.DOM/project2/code/app.js
@@ -46,10 +46,27 @@ const removeItemButton = document.querySelector('button.removeItemButton');
 
 listUl.addEventListener('click', (event) => {
   if (event.target.tagName == 'BUTTON') {
+    if (event.target.className == 'remove') {
+      let li = event.target.parentNode;
+      let ul = li.parentNode;
+      ul.removeChild(li);
+    }
+   if (event.target.className == 'up') {
      let li = event.target.parentNode;
-    let ul = li.parentNode;
-    ul.removeChild(li);
-    
+     let prevLi = li.previousElementSibling;
+     let ul = li.parentNode;
+     if (prevLi) {
+        ul.insertBefore(li, prevLi);
+     }
+    }
+   if (event.target.className == 'down') {
+     let li = event.target.parentNode;
+     let nextLi = li.nextElementSibling;
+     let ul = li.parentNode;
+     if (nextLi) {
+        ul.insertBefore(mextLi, li);
+     }
+    }
   }
 });
 
