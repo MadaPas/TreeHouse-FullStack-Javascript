1a15c1a083ecf9a6e8c0ed9bf57b0fc7cb807f09
Madalina - Andreea <mada0193@stud.kea.dk>
2020-12-30 11:10:16 +0100

    DOM - task-8 - last one

---
+++ 3.DOM/project2/task-8/app.js
@@ -0,0 +1,8 @@
+var section = document.querySelector('section');
+var paragraphs = section.children;
+
+for(var i = 0; i < paragraphs.length;  i = i + 1) {
+
+ paragraphs[i].style.color = 'blue'; 
+
+}
